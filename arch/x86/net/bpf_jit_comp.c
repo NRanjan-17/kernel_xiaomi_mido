2dc8dd64c5b1149bcebc0f0ef34dbc48abf64483
Daniel Borkmann <daniel@iogearbox.net>
2017-02-16 22:24:49 +0100

    bpf: remove stubs for cBPF from arch code
    Remove the dummy bpf_jit_compile() stubs for eBPF JITs and make
    that a single __weak function in the core that can be overridden
    similarly to the eBPF one. Also remove stale pr_err() mentions
    of bpf_jit_compile.
    
    Change-Id: Iac221c09e9ae0879acdd7064d710c4f7cb8f478d
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/x86/net/bpf_jit_comp.c
+++ arch/x86/net/bpf_jit_comp.c
@@ -1070,7 +1070,7 @@ common_load:
 
 		ilen = prog - temp;
 		if (ilen > BPF_MAX_INSN_SIZE) {
-			pr_err("bpf_jit_compile fatal insn size error\n");
+			pr_err("bpf_jit: fatal insn size error\n");
 			return -EFAULT;
 		}
 
@@ -1095,10 +1095,6 @@ common_load:
 		prog = temp;
 	}
 	return proglen;
-}
-
-void bpf_jit_compile(struct bpf_prog *prog)
-{
 }
 
 struct bpf_prog *bpf_int_jit_compile(struct bpf_prog *prog)
