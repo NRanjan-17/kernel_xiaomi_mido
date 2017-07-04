88764fcfe95c6b081ae3ce351029e845e331e34d
Colin Ian King <colin.king@canonical.com>
2017-07-04 16:21:12 +0100

    bpf: add missing break in for the TCP_BPF_SNDCWND_CLAMP case
    There appears to be a missing break in the TCP_BPF_SNDCWND_CLAMP case.
    Currently the non-error path where val is greater than zero falls through
    to the default case that sets the error return to -EINVAL. Add in
    the missing break.
    
    Detected by CoverityScan, CID#1449376 ("Missing break in switch")
    
    Fixes: 13bf96411ad2 ("bpf: Adds support for setting sndcwnd clamp")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3107,6 +3107,7 @@ BPF_CALL_5(bpf_setsockopt, struct bpf_sock_ops_kern *, bpf_sock,
 					tp->snd_cwnd_clamp = val;
 					tp->snd_ssthresh = val;
 				}
+				break;
 			default:
 				ret = -EINVAL;
 			}
