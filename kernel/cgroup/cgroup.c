c85984b6fbcdcd9befda36e59a837547d4961976
Tejun Heo <tj@kernel.org>
2017-08-11 05:49:01 -0700

    cgroup: misc changes
    Misc trivial changes to prepare for future changes.  No functional
    difference.
    
    * Expose cgroup_get(), cgroup_tryget() and cgroup_parent().
    
    * Implement task_dfl_cgroup() which dereferences css_set->dfl_cgrp.
    
    * Rename cgroup_stats_show() to cgroup_stat_show() for consistency
      with the file name.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -3407,7 +3407,7 @@ static void cgroup_pressure_release(struct kernfs_open_file *of)
 }
 #endif /* CONFIG_PSI */
 
-static int cgroup_stats_show(struct seq_file *seq, void *v)
+static int cgroup_stat_show(struct seq_file *seq, void *v)
 {
 	struct cgroup *cgroup = seq_css(seq)->cgroup;
 
@@ -4562,7 +4562,7 @@ static struct cftype cgroup_base_files[] = {
 	},
 	{
 		.name = "cgroup.stat",
-		.seq_show = cgroup_stats_show,
+		.seq_show = cgroup_stat_show,
 	},
 	{ }	/* terminate */
 };
