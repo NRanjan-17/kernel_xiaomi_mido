6c943e55daa1f646edf15e2b7a1c5e46602db0ae
John Fastabend <john.fastabend@gmail.com>
2017-08-16 15:02:32 -0700

    bpf: sock_map fixes for !CONFIG_BPF_SYSCALL and !STREAM_PARSER
    Resolve issues with !CONFIG_BPF_SYSCALL and !STREAM_PARSER
    
    net/core/filter.c: In function ‘do_sk_redirect_map’:
    net/core/filter.c:1881:3: error: implicit declaration of function ‘__sock_map_lookup_elem’ [-Werror=implicit-function-declaration]
       sk = __sock_map_lookup_elem(ri->map, ri->ifindex);
       ^
    net/core/filter.c:1881:6: warning: assignment makes pointer from integer without a cast [enabled by default]
       sk = __sock_map_lookup_elem(ri->map, ri->ifindex);
    
    Fixes: 174a79ff9515 ("bpf: sockmap with sk redirect support")
    Reported-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -405,7 +405,6 @@ struct bpf_prog *bpf_prog_get_type_path(const char *name, enum bpf_prog_type typ
 /* Map specifics */
 struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
 struct net_device  *__dev_map_hash_lookup_elem(struct bpf_map *map, u32 key);
-struct sock  *__sock_map_lookup_elem(struct bpf_map *map, u32 key);
 void __dev_map_insert_ctx(struct bpf_map *map, u32 index);
 void __dev_map_flush(struct bpf_map *map);
 
@@ -518,6 +517,15 @@ static inline bool bpf_prog_is_dev_bound(struct bpf_prog_aux *aux)
 	return false;
 }
 #endif /* CONFIG_NET && CONFIG_BPF_SYSCALL */
+
+#if defined(CONFIG_STREAM_PARSER) && defined(CONFIG_BPF_SYSCALL)
+struct sock  *__sock_map_lookup_elem(struct bpf_map *map, u32 key);
+#else
+static inline struct sock  *__sock_map_lookup_elem(struct bpf_map *map, u32 key)
+{
+	return NULL;
+}
+#endif
 
 /* verifier prototypes for helper functions called from eBPF programs */
 extern const struct bpf_func_proto bpf_map_lookup_elem_proto;
