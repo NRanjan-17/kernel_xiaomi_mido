2d7a6447cc253d00c048077d77321c695288df37
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-10-09 10:30:14 -0700

    bpf: don't rely on the verifier lock for metadata_dst allocation
    bpf_skb_set_tunnel_*() functions require allocation of per-cpu
    metadata_dst.  The allocation happens upon verification of the
    first program using those helpers.  In preparation for removing
    the verifier lock, use cmpxchg() to make sure we only allocate
    the metadata_dsts once.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dst.c
+++ net/core/dst.c
@@ -425,6 +425,22 @@ struct metadata_dst __percpu *metadata_dst_alloc_percpu(u8 optslen, gfp_t flags)
 }
 EXPORT_SYMBOL_GPL(metadata_dst_alloc_percpu);
 
+void metadata_dst_free_percpu(struct metadata_dst __percpu *md_dst)
+{
+	int cpu;
+
+#ifdef CONFIG_DST_CACHE
+	for_each_possible_cpu(cpu) {
+		struct metadata_dst *one_md_dst = per_cpu_ptr(md_dst, cpu);
+
+		if (one_md_dst->type == METADATA_IP_TUNNEL)
+			dst_cache_destroy(&one_md_dst->u.tun_info.dst_cache);
+	}
+#endif
+	free_percpu(md_dst);
+}
+EXPORT_SYMBOL_GPL(metadata_dst_free_percpu);
+
 /* Dirty hack. We did it in 2.2 (in __dst_free),
  * we have _very_ good reasons not to repeat
  * this mistake in 2.3, but we have no choice
