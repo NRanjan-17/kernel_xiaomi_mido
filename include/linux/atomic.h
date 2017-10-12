94e6039b276c3b8c87b43ac11a637f5cb04ba8e7
Will Deacon <will.deacon@arm.com>
2017-10-12 13:20:48 +0100

    locking/atomic: Add atomic_cond_read_acquire()
    smp_cond_load_acquire() provides a way to spin on a variable with acquire
    semantics until some conditional expression involving the variable is
    satisfied. Architectures such as arm64 can potentially enter a low-power
    state, waking up only when the value of the variable changes, which
    reduces the system impact of tight polling loops.
    
    This patch makes the same interface available to users of atomic_t,
    atomic64_t and atomic_long_t, rather than require messy accesses to the
    structure internals.
    
    Change-Id: I5ef2f84dfe810b4e4d26bbb0617c2070f4ccb678
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Jeremy.Linton@arm.com
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Waiman Long <longman@redhat.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: http://lkml.kernel.org/r/1507810851-306-3-git-send-email-will.deacon@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Git-commit: 4df714be4dcf40bfb0d4af0f851a6e1977afa02e
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- include/linux/atomic.h
+++ include/linux/atomic.h
@@ -630,6 +630,8 @@ static inline int atomic_dec_if_positive(atomic_t *v)
 #define atomic_cond_read_relaxed(v, c)	smp_cond_load_relaxed(&(v)->counter, (c))
 #define atomic_cond_read_acquire(v, c)	smp_cond_load_acquire(&(v)->counter, (c))
 
+#define atomic_cond_read_acquire(v, c)	smp_cond_load_acquire(&(v)->counter, (c))
+
 #ifdef CONFIG_GENERIC_ATOMIC64
 #include <asm-generic/atomic64.h>
 #endif
