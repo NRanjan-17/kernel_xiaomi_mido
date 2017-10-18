98463aa0f3e627f6a82b0be9f041134973a46b09
John Fastabend <john.fastabend@gmail.com>
2017-10-18 07:11:22 -0700

    bpf: require CAP_NET_ADMIN when using sockmap maps
    Restrict sockmap to CAP_NET_ADMIN.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/sockmap.c
+++ kernel/bpf/sockmap.c
@@ -489,6 +489,9 @@ static struct bpf_map *sock_map_alloc(union bpf_attr *attr)
 	int err = -EINVAL;
 	u64 cost;
 
+	if (!capable(CAP_NET_ADMIN))
+		return ERR_PTR(-EPERM);
+
 	/* check sanity of attributes */
 	if (attr->max_entries == 0 || attr->key_size != 4 ||
 	    attr->value_size != 4 || attr->map_flags & ~SOCK_CREATE_FLAG_MASK)
