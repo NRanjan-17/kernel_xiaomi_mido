cfe2112071ac4b573a9beb88a1dd2d8c09742373
Arnd Bergmann <arnd@arndb.de>
2017-11-02 12:05:52 +0100

    bpf: remove the verifier ops from program structure
    Since the verifier ops don't have to be associated with
    the program for its entire lifetime we can move it to
    verifier's struct bpf_verifier_env.
    
    [huexxx@gmail.com: adapt]
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -809,15 +809,6 @@ static const struct bpf_prog_ops * const bpf_prog_types[] = {
 #undef BPF_MAP_TYPE
 };
 
-static const struct bpf_verifier_ops * const bpf_verifier_ops[] = {
-#define BPF_PROG_TYPE(_id, _name) \
-	[_id] = & _name ## _verifier_ops,
-#define BPF_MAP_TYPE(_id, _ops)
-#include <linux/bpf_types.h>
-#undef BPF_PROG_TYPE
-#undef BPF_MAP_TYPE
-};
-
 static int find_prog_type(enum bpf_prog_type type, struct bpf_prog *prog)
 {
 	if (type >= ARRAY_SIZE(bpf_prog_types) || !bpf_prog_types[type])
@@ -827,7 +818,6 @@ static int find_prog_type(enum bpf_prog_type type, struct bpf_prog *prog)
 		prog->aux->ops = bpf_prog_types[type];
 	else
 		prog->aux->ops = &bpf_offload_prog_ops;
-	prog->aux->vops = bpf_verifier_ops[type];
 	prog->type = type;
 	return 0;
 }
