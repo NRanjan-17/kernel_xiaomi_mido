c081e9355c625d980563ed532522ddf26805bf5f
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-11-20 15:21:55 -0800

    net: xdp: don't allow device-bound programs in driver mode
    Currently device-bound programs are not able to run on the host
    to save resources (host JIT is not invoked).  Don't allow XDP
    programs to be attached without the HW_MODE flag.  In theory
    if program is already translated for device offload the driver
    should choose to offload it instead of loading it in the driver.
    However, offloading translated program may still fail resulting
    in device-bound program being run on the host.
    
    Prevent this by refusing to attach device bound programs if
    XDP_FLAGS_HW_MODE is not set.
    
    [huexxx@gmail.com: avoid build error, use pr_err() instead of NL_SET_ERR_MSG()]
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -7076,6 +7076,13 @@ int dev_change_xdp_fd(struct net_device *dev, int fd)
 		prog = bpf_prog_get_type(fd, BPF_PROG_TYPE_XDP);
 		if (IS_ERR(prog))
 			return PTR_ERR(prog);
+
+		if (!(flags & XDP_FLAGS_HW_MODE) &&
+		    bpf_prog_is_dev_bound(prog->aux)) {
+			pr_err("using device-bound program without HW_MODE flag is not supported");
+			bpf_prog_put(prog);
+			return -EINVAL;
+		}
 	}
 
 	xdp.command = XDP_SETUP_PROG;
