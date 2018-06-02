132526acd46a318c0cb6dbc09226e81cfb484f75
Daniel Borkmann <daniel@iogearbox.net>
2018-06-02 23:06:35 +0200

    bpf: avoid retpoline for lookup/update/delete calls on maps
    While some of the BPF map lookup helpers provide a ->map_gen_lookup()
    callback for inlining the map lookup altogether it is not available
    for every map, so the remaining ones have to call bpf_map_lookup_elem()
    helper which does a dispatch to map->ops->map_lookup_elem(). In
    times of retpolines, this will control and trap speculative execution
    rather than letting it do its work for the indirect call and will
    therefore cause a slowdown. Likewise, bpf_map_update_elem() and
    bpf_map_delete_elem() do not have an inlined version and need to call
    into their map->ops->map_update_elem() resp. map->ops->map_delete_elem()
    handlers.
    
    Before:
    
      # bpftool prog dump xlated id 1
        0: (bf) r2 = r10
        1: (07) r2 += -8
        2: (7a) *(u64 *)(r2 +0) = 0
        3: (18) r1 = map[id:1]
        5: (85) call __htab_map_lookup_elem#232656
        6: (15) if r0 == 0x0 goto pc+4
        7: (71) r1 = *(u8 *)(r0 +35)
        8: (55) if r1 != 0x0 goto pc+1
        9: (72) *(u8 *)(r0 +35) = 1
       10: (07) r0 += 56
       11: (15) if r0 == 0x0 goto pc+4
       12: (bf) r2 = r0
       13: (18) r1 = map[id:1]
       15: (85) call bpf_map_delete_elem#215008  <-- indirect call via
       16: (95) exit                                 helper
    
    After:
    
      # bpftool prog dump xlated id 1
        0: (bf) r2 = r10
        1: (07) r2 += -8
        2: (7a) *(u64 *)(r2 +0) = 0
        3: (18) r1 = map[id:1]
        5: (85) call __htab_map_lookup_elem#233328
        6: (15) if r0 == 0x0 goto pc+4
        7: (71) r1 = *(u8 *)(r0 +35)
        8: (55) if r1 != 0x0 goto pc+1
        9: (72) *(u8 *)(r0 +35) = 1
       10: (07) r0 += 56
       11: (15) if r0 == 0x0 goto pc+4
       12: (bf) r2 = r0
       13: (18) r1 = map[id:1]
       15: (85) call htab_lru_map_delete_elem#238240  <-- direct call
       16: (95) exit
    
    In all three lookup/update/delete cases however we can use the actual
    address of the map callback directly if we find that there's only a
    single path with a map pointer leading to the helper call, meaning
    when the map pointer has not been poisoned from verifier side.
    Example code can be seen above for the delete case.
    
    [huexxx@gmail.com: miss htab_lru_map_gen_lookup() on kernel/bpf/hashtab.c]
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/hashtab.c
+++ kernel/bpf/hashtab.c
@@ -505,7 +505,9 @@ static u32 htab_map_gen_lookup(struct bpf_map *map, struct bpf_insn *insn_buf)
 	struct bpf_insn *insn = insn_buf;
 	const int ret = BPF_REG_0;
 
-	*insn++ = BPF_EMIT_CALL((u64 (*)(u64, u64, u64, u64, u64))__htab_map_lookup_elem);
+	BUILD_BUG_ON(!__same_type(&__htab_map_lookup_elem,
+		     (void *(*)(struct bpf_map *map, void *key))NULL));
+	*insn++ = BPF_EMIT_CALL(BPF_CAST_CALL(__htab_map_lookup_elem));
 	*insn++ = BPF_JMP_IMM(BPF_JEQ, ret, 0, 1);
 	*insn++ = BPF_ALU64_IMM(BPF_ADD, ret,
 				offsetof(struct htab_elem, key) +
@@ -1339,7 +1341,9 @@ static u32 htab_of_map_gen_lookup(struct bpf_map *map,
 	struct bpf_insn *insn = insn_buf;
 	const int ret = BPF_REG_0;
 
-	*insn++ = BPF_EMIT_CALL((u64 (*)(u64, u64, u64, u64, u64))__htab_map_lookup_elem);
+	BUILD_BUG_ON(!__same_type(&__htab_map_lookup_elem,
+		     (void *(*)(struct bpf_map *map, void *key))NULL));
+	*insn++ = BPF_EMIT_CALL(BPF_CAST_CALL(__htab_map_lookup_elem));
 	*insn++ = BPF_JMP_IMM(BPF_JEQ, ret, 0, 2);
 	*insn++ = BPF_ALU64_IMM(BPF_ADD, ret,
 				offsetof(struct htab_elem, key) +
