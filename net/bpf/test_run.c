5c5256d181500e649f7d287a86a9453ae7f6237e
Song Liu <songliubraving@fb.com>
2018-10-19 09:57:58 -0700

    UPSTREAM: bpf: add tests for direct packet access from CGROUP_SKB
    Tests are added to make sure CGROUP_SKB cannot access:
      tc_classid, data_meta, flow_keys
    
    and can read and write:
      mark, prority, and cb[0-4]
    
    and can read other fields.
    
    To make selftest with skb->sk work, a dummy sk is added in
    bpf_prog_test_run_skb().
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -10,6 +10,8 @@
 #include <linux/etherdevice.h>
 #include <linux/filter.h>
 #include <linux/sched.h>
+#include <net/sock.h>
+#include <net/tcp.h>
 
 static __always_inline u32 bpf_test_run_one(struct bpf_prog *prog, void *ctx)
 {
@@ -99,6 +101,7 @@ int bpf_prog_test_run_skb(struct bpf_prog *prog, const union bpf_attr *kattr,
 	u32 retval, duration;
 	int hh_len = ETH_HLEN;
 	struct sk_buff *skb;
+	struct sock *sk;
 	void *data;
 	int ret;
 
@@ -121,11 +124,21 @@ int bpf_prog_test_run_skb(struct bpf_prog *prog, const union bpf_attr *kattr,
 		break;
 	}
 
-	skb = build_skb(data, 0);
-	if (!skb) {
+	sk = kzalloc(sizeof(struct sock), GFP_USER);
+	if (!sk) {
 		kfree(data);
 		return -ENOMEM;
 	}
+	sock_net_set(sk, current->nsproxy->net_ns);
+	sock_init_data(NULL, sk);
+
+	skb = build_skb(data, 0);
+	if (!skb) {
+		kfree(data);
+		kfree(sk);
+		return -ENOMEM;
+	}
+	skb->sk = sk;
 
 	skb_reserve(skb, NET_SKB_PAD + NET_IP_ALIGN);
 	__skb_put(skb, size);
@@ -155,6 +168,7 @@ int bpf_prog_test_run_skb(struct bpf_prog *prog, const union bpf_attr *kattr,
 		size = skb_headlen(skb);
 	ret = bpf_test_finish(kattr, uattr, skb->data, size, retval, duration);
 	kfree_skb(skb);
+    kfree(sk);
 	return ret;
 }
 
