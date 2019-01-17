466d2ec35ba8351b3281ccf31d80b716db12e187
Song Liu <songliubraving@fb.com>
2019-01-17 08:15:21 -0800

    bpf: Add module name [bpf] to ksymbols for bpf programs
    With this patch, /proc/kallsyms will show BPF programs as
    
      <addr> t bpf_prog_<tag>_<name> [bpf]
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Reviewed-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: kernel-team@fb.com
    Cc: netdev@vger.kernel.org
    Link: http://lkml.kernel.org/r/20190117161521.1341602-10-songliubraving@fb.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -541,7 +541,7 @@ static int get_ksymbol_mod(struct kallsym_iter *iter)
 
 static int get_ksymbol_bpf(struct kallsym_iter *iter)
 {
-	iter->module_name[0] = '\0';
+	strlcpy(iter->module_name, "bpf", MODULE_NAME_LEN);
 	iter->exported = 0;
 	return bpf_get_kallsym(iter->pos - iter->pos_mod_end,
 			       &iter->value, &iter->type,
