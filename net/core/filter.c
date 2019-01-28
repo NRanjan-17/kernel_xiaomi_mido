5cd7dce51a6d64335e8f56bd72fff823b5fbc87d
Stanislav Fomichev <sdf@google.com>
2019-01-28 08:53:54 -0800

    UPSTREAM: bpf: add BPF_PROG_TEST_RUN support for flow dissector
    The input is packet data, the output is struct bpf_flow_key. This should
    make it easy to test flow dissector programs without elaborate
    setup.
    
    Change-Id: I8639560bc8b1ce6b983365bb317b184b2cc3ab8a
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -6682,6 +6682,7 @@ const struct bpf_verifier_ops flow_dissector_verifier_ops = {
 };
  
 const struct bpf_prog_ops flow_dissector_prog_ops = {
+	.test_run		= bpf_prog_test_run_flow_dissector,
 };
 
 const struct bpf_verifier_ops sk_msg_verifier_ops = {
