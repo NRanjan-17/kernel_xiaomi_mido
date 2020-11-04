76991b90594dd984b2bc3a778eabe5873be9506f
David Verbeiren <david.verbeiren@tessares.net>
2020-11-04 12:23:32 +0100

    UPSTREAM: bpf: Zero-fill re-used per-cpu map element
    [ Upstream commit d3bec0138bfbe58606fc1d6f57a4cdc1a20218db ]
    
    Zero-fill element values for all other cpus than current, just as
    when not using prealloc. This is the only way the bpf program can
    ensure known initial values for all cpus ('onallcpus' cannot be
    set when coming from the bpf program).
    
    The scenario is: bpf program inserts some elements in a per-cpu
    map, then deletes some (or userspace does). When later adding
    new elements using bpf_map_update_elem(), the bpf program can
    only set the value of the new elements for the current cpu.
    When prealloc is enabled, previously deleted elements are re-used.
    Without the fix, values for other cpus remain whatever they were
    when the re-used entry was previously freed.
    
    A selftest is added to validate correct operation in above
    scenario as well as in case of LRU per-cpu map element re-use.
    
    Fixes: 6c9059817432 ("bpf: pre-allocate hash map elements")
    Change-Id: I51fa789725ab02ee5eaf1c04bd8519135accf6a7
    Signed-off-by: David Verbeiren <david.verbeiren@tessares.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20201104112332.15191-1-david.verbeiren@tessares.net
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/hashtab.c
+++ kernel/bpf/hashtab.c
@@ -681,6 +681,33 @@ static void pcpu_copy_value(struct bpf_htab *htab, void __percpu *pptr,
 	}
 }
 
+
+static void pcpu_init_value(struct bpf_htab *htab, void __percpu *pptr,
+			    void *value, bool onallcpus)
+{
+	/* When using prealloc and not setting the initial value on all cpus,
+	 * zero-fill element values for other cpus (just as what happens when
+	 * not using prealloc). Otherwise, bpf program has no way to ensure
+	 * known initial values for cpus other than current one
+	 * (onallcpus=false always when coming from bpf prog).
+	 */
+	if (htab_is_prealloc(htab) && !onallcpus) {
+		u32 size = round_up(htab->map.value_size, 8);
+		int current_cpu = raw_smp_processor_id();
+		int cpu;
+
+		for_each_possible_cpu(cpu) {
+			if (cpu == current_cpu)
+				bpf_long_memcpy(per_cpu_ptr(pptr, cpu), value,
+						size);
+			else
+				memset(per_cpu_ptr(pptr, cpu), 0, size);
+		}
+	} else {
+		pcpu_init_value(htab, pptr, value, onallcpus);
+	}
+}
+
 static bool fd_htab_map_needs_adjust(const struct bpf_htab *htab)
 {
 	return htab->map.map_type == BPF_MAP_TYPE_HASH_OF_MAPS &&
@@ -747,7 +774,7 @@ static struct htab_elem *alloc_htab_elem(struct bpf_htab *htab, void *key,
 			}
 		}
 
-		pcpu_copy_value(htab, pptr, value, onallcpus);
+		pcpu_init_value(htab, pptr, value, onallcpus);
 
 		if (!prealloc)
 			htab_elem_set_ptr(l_new, key_size, pptr);
