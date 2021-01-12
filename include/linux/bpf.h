b2603230da24acf8fc4c57b1ef2e0a18a611a5b9
Andrii Nakryiko <andrii@kernel.org>
2021-01-11 23:55:14 -0800

    bpf: Add bpf_patch_call_args prototype to include/linux/bpf.h
    Add bpf_patch_call_args() prototype. This function is called from BPF verifier
    and only if CONFIG_BPF_JIT_ALWAYS_ON is not defined. This fixes compiler
    warning about missing prototype in some kernel configurations.
    
    Fixes: 1ea47e01ad6e ("bpf: add support for bpf_call to interpreter")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20210112075520.4103414-2-andrii@kernel.org

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -593,7 +593,10 @@ static inline void bpf_long_memcpy(void *dst, const void *src, u32 size)
 /* verify correctness of eBPF program */
 int bpf_check(struct bpf_prog **fp, union bpf_attr *attr,
 	      union bpf_attr __user *uattr);
+
+#ifndef CONFIG_BPF_JIT_ALWAYS_ON
 void bpf_patch_call_args(struct bpf_insn *insn, u32 stack_depth);
+#endif
 
 struct bpf_prog *bpf_prog_get_type_path(const char *name, enum bpf_prog_type type);
 
