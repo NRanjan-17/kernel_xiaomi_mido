ccd70b00559f2da92f99dfb0e082f8e38856767b
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-05-22 21:11:27 +0200

    fixup! ebpf: allow bpf_get_current_uid_gid_proto also for networking
    * Keep it on sk_filter_func_proto()

--- net/core/filter.c
+++ net/core/filter.c
@@ -3155,8 +3155,6 @@ bpf_base_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 	case BPF_FUNC_trace_printk:
 		if (capable(CAP_SYS_ADMIN))
 			return bpf_get_trace_printk_proto();
-	case BPF_FUNC_get_current_uid_gid:
-		return &bpf_get_current_uid_gid_proto;
 	default:
 		return NULL;
 	}
@@ -3196,6 +3194,8 @@ sk_filter_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_get_socket_cookie_proto;
 	case BPF_FUNC_get_socket_uid:
 		return &bpf_get_socket_uid_proto;
+	case BPF_FUNC_get_current_uid_gid:
+		return &bpf_get_current_uid_gid_proto;
 	default:
 		return bpf_base_func_proto(func_id, prog);
 	}
