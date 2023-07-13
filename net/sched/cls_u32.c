835d5c93a85b7f4ef9a5f5de562b42f400cab0b5
Victor Nogueira <victor@mojatatu.com>
2023-07-13 20:05:12 +0200

    net: sched: cls_u32: Undo refcount decrement in case update failed
    In the case of an update, when TCA_U32_LINK is set, u32_set_parms will
    decrement the refcount of the ht_down (struct tc_u_hnode) pointer
    present in the older u32 filter which we are replacing. However, if
    u32_replace_hw_knode errors out, the update command fails and that
    ht_down pointer continues decremented. To fix that, when
    u32_replace_hw_knode fails, check if ht_down's refcount was decremented
    and undo the decrement.
    
    [huexxx@gmail.com: adapt]
    Fixes: d34e3e181395 ("net: cls_u32: Add support for skip-sw flag to tc u32 classifier.")
    Signed-off-by: Victor Nogueira <victor@mojatatu.com>
    Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Reviewed-by: Pedro Tammela <pctammela@mojatatu.com>
    Reviewed-by: Simon Horman <simon.horman@corigine.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_u32.c
+++ net/sched/cls_u32.c
@@ -910,6 +910,13 @@ static int u32_change(struct net *net, struct sk_buff *in_skb,
 		if (err) {
 			u32_unbind_filter(tp, new, tb);
 
+			if (tb[TCA_U32_LINK]) {
+				struct tc_u_hnode *ht_old;
+
+				ht_old = rtnl_dereference(n->ht_down);
+				if (ht_old)
+					ht_old->refcnt++;
+			}
 			u32_destroy_key(tp, new, false);
 			return err;
 		}
