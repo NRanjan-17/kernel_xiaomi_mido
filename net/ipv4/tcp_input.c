9e069ecb592982fc597c2d5a4cdeacec13feaa06
yyq <3531236823@qq.com>
2025-01-18 11:10:15 +0800

    tcp: Function for sending SYN-ACK packets

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6568,9 +6568,8 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 		tcp_rsk(req)->tfo_listener = false;
 		if (!want_cookie)
 			inet_csk_reqsk_queue_hash_add(sk, req, TCP_TIMEOUT_INIT);
-		af_ops->send_synack(sk, dst, &fl, req, &foc,
-				    !want_cookie ? TCP_SYNACK_NORMAL :
-						   TCP_SYNACK_COOKIE);
+		af_ops->send_synack(sk, dst, &fl, req,
+				    &foc, !want_cookie);
 		if (want_cookie) {
 			reqsk_free(req);
 			return 0;
