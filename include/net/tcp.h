5ba19258f4d623692a70080ae4f51e92efde058d
yyq <3531236823@qq.com>
2025-01-18 13:23:10 +0800

    bpf: Move the tcp_ulc_ops member upwards

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2121,11 +2121,12 @@ enum {
 
 struct tcp_ulp_ops {
 	struct list_head	list;
+
 	/* initialize ulp */
 	int (*init)(struct sock *sk);
-
 	/* cleanup ulp */
 	void (*release)(struct sock *sk);
+
 	char		name[TCP_ULP_NAME_MAX];
 	struct module	*owner;
 };
