04f7569c7f745aaab232f6c5b270562a084841f8
Kuniyuki Iwashima <kuniyu@google.com>
2025-07-17 19:46:43 +0000

    BACKPORT: memcg: convert memcg->socket_pressure to u64
    memcg->socket_pressure is initialised with jiffies when the memcg is
    created.
    
    Once vmpressure detects that the cgroup is under memory pressure, the
    field is updated with jiffies + HZ to signal the fact to the socket layer
    and suppress memory allocation for one second.
    
    Otherwise, the field is not updated.
    
    mem_cgroup_under_socket_pressure() uses time_before() to check if jiffies
    is less than memcg->socket_pressure, and this has a bug on 32-bit kernel.
    
      if (time_before(jiffies, memcg->socket_pressure))
              return true;
    
    As time_before() casts the final result to long, the acceptable delta
    between two timestamps is 2 ^ (BITS_PER_LONG - 1).
    
    On 32-bit kernel with CONFIG_HZ=1000, this is about 24 days.
    
      >>> (2 ** 31) / 1000 / 60 / 60 / 24
      24.855134814814818
    
    Once 24 days have passed since the last update of socket_pressure,
    mem_cgroup_under_socket_pressure() starts to lie until the next 24 days
    pass.
    
    We don't need to worry about this on 64-bit machines unless they serve for
    300 million years.
    
      >>> (2 ** 63) / 1000 / 60 / 60 / 24 / 365
      292471208.6775361
    
    Let's convert memcg->socket_pressure to u64.
    
    Performance teting:
    
    I don't have a real 32-bit machine so this is a result on QEMU, but
    with/without the u64 jiffie patch, the time spent in
    mem_cgroup_under_socket_pressure() was 1~5us and I didn't see any
    measurable delta.
    
    no patch applied:
    iperf3   273 [000]   137.296248:
    probe:mem_cgroup_under_socket_pressure: (c13660d0)
                    c13660d1 mem_cgroup_under_socket_pressure+0x1
    ([kernel.kallsyms])
    iperf3   273 [000]   137.296249:
    probe:mem_cgroup_under_socket_pressure__return: (c13660d0 <- c1d8fd7f)
    iperf3   273 [000]   137.296251:
    probe:mem_cgroup_under_socket_pressure: (c13660d0)
                    c13660d1 mem_cgroup_under_socket_pressure+0x1
    ([kernel.kallsyms])
    iperf3   273 [000]   137.296253:
    probe:mem_cgroup_under_socket_pressure__return: (c13660d0 <- c1d8fd7f)
    
    u64 jiffies patch applied:
    iperf3   308 [001]   330.669370:
    probe:mem_cgroup_under_socket_pressure: (c12ddba0)
                    c12ddba1 mem_cgroup_under_socket_pressure+0x1
    ([kernel.kallsyms])
    iperf3   308 [001]   330.669371:
    probe:mem_cgroup_under_socket_pressure__return: (c12ddba0 <- c1ce98bf)
    iperf3   308 [001]   330.669382:
    probe:mem_cgroup_under_socket_pressure: (c12ddba0)
                    c12ddba1 mem_cgroup_under_socket_pressure+0x1
    ([kernel.kallsyms])
    iperf3   308 [001]   330.669384:
    probe:mem_cgroup_under_socket_pressure__return: (c12ddba0 <- c1ce98bf)
    
    So the u64 approach is good enough.
    
    Link: https://lkml.kernel.org/r/20250717194645.1096500-1-kuniyu@google.com
    Fixes: 8e8ae645249b ("mm: memcontrol: hook up vmpressure to socket pressure")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reported-by: Neal Cardwell <ncardwell@google.com>
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Eric Dumazet <ncardwell@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4259,7 +4259,10 @@ static struct mem_cgroup *mem_cgroup_alloc(void)
 	vmpressure_init(&memcg->vmpressure);
 	INIT_LIST_HEAD(&memcg->event_list);
 	spin_lock_init(&memcg->event_list_lock);
-	memcg->socket_pressure = jiffies;
+	memcg->socket_pressure = get_jiffies_64();
+#if BITS_PER_LONG < 64
+	seqlock_init(&memcg->socket_pressure_seqlock);
+#endif
 #ifndef CONFIG_SLOB
 	memcg->kmemcg_id = -1;
 #endif
