2116287918f3dad5980a121f14fde6f6ce0f656d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-12 18:11:21 +0800

    Kbuild: link-vmlinux.sh: restore to 421929c4666fc9f6847bc59e7fd4cd5ab70c5ffd

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -33,7 +33,7 @@ set -e
 info()
 {
 	if [ "${quiet}" != "silent_" ]; then
-		printf "  %-7s %s\n" "${1}" "${2}"
+		printf "  %-7s %s\n" ${1} ${2}
 	fi
 }
 
@@ -128,8 +128,8 @@ recordmcount()
 }
 
 # Link of vmlinux
-# ${1} - output file
-# ${@:2} - optional extra .o files
+# ${1} - optional extra .o files
+# ${2} - output file
 vmlinux_link()
 {
 	local lds="${objtree}/${KBUILD_LDS}"
@@ -151,7 +151,7 @@ vmlinux_link()
 				--start-group				\
 				${KBUILD_VMLINUX_MAIN}			\
 				--end-group				\
-				${@:2}"
+				${1}"
 		fi
 
 		${ld} ${ldflags} -o ${2} -T ${lds} ${objects}
@@ -163,49 +163,15 @@ vmlinux_link()
 				-Wl,--start-group			\
 				${KBUILD_VMLINUX_MAIN}			\
 				-Wl,--end-group				\
-				${@:2}"
+				${1}"
 		fi
 
-		${CC} ${CFLAGS_vmlinux} -o ${1}				\
+		${CC} ${CFLAGS_vmlinux} -o ${2}				\
 			-Wl,-T,${lds}					\
 			${objects}					\
 			-lutil -lrt -lpthread
 		rm -f linux
 	fi
-}
-
-# generate .BTF typeinfo from DWARF debuginfo
-# ${1} - vmlinux image
-# ${2} - file to dump raw BTF data into
-gen_btf()
-{
-	local pahole_ver
-
-	if ! [ -x "$(command -v ${PAHOLE})" ]; then
-		info "BTF" "${1}: pahole (${PAHOLE}) is not available"
-		return 1
-	fi
-
-	pahole_ver=$(${PAHOLE} --version | sed -E 's/v([0-9]+)\.([0-9]+)/\1\2/')
-	if [ "${pahole_ver}" -lt "113" ]; then
-		info "BTF" "${1}: pahole version $(${PAHOLE} --version) is too old, need at least v1.13"
-		return 1
-	fi
-
-	info "BTF" ${2}
-	vmlinux_link ${1}
-	LLVM_OBJCOPY=${OBJCOPY} ${PAHOLE} -J ${1}
-
-	# Create ${2} which contains just .BTF section but no symbols. Add
-	# SHF_ALLOC because .BTF will be part of the vmlinux image. --strip-all
-	# deletes all symbols including __start_BTF and __stop_BTF, which will
-	# be redefined in the linker script. Add 2>/dev/null to suppress GNU
-	# objcopy warnings: "empty loadable segment detected at ..."
-	${OBJCOPY} --only-section=.BTF --set-section-flags .BTF=alloc,readonly \
-		--strip-all ${1} ${2} 2>/dev/null
-	# Change e_type to ET_REL so that it can be used to link final vmlinux.
-	# Unlike GNU ld, lld does not allow an ET_EXEC input.
-	printf '\1' | dd of=${2} conv=notrunc bs=1 seek=16 status=none
 }
 
 # Create ${2} .o file with all symbols from the ${1} object file
@@ -279,7 +245,6 @@ sortextable()
 # Delete output files in case of error
 cleanup()
 {
-	rm -f .btf.*
 	rm -f .old_version
 	rm -f .tmp_System.map
 	rm -f .tmp_kallsyms*
@@ -372,13 +337,6 @@ if [ ! -z ${RTIC_MPGEN+x} ]; then
 	KBUILD_VMLINUX_MAIN+=$RTIC_MP_O
 fi
 
-btf_vmlinux_bin_o=""
-if [ -n "${CONFIG_DEBUG_INFO_BTF}" ]; then
-	if gen_btf .tmp_vmlinux.btf .btf.vmlinux.bin.o ; then
-		btf_vmlinux_bin_o=.btf.vmlinux.bin.o
-	fi
-fi
-
 kallsymso=""
 kallsyms_vmlinux=""
 if [ -n "${CONFIG_KALLSYMS}" ]; then
@@ -410,11 +368,11 @@ if [ -n "${CONFIG_KALLSYMS}" ]; then
 	kallsyms_vmlinux=.tmp_vmlinux2
 
 	# step 1
-	vmlinux_link .tmp_vmlinux1 ${btf_vmlinux_bin_o}
+	vmlinux_link "" .tmp_vmlinux1
 	kallsyms .tmp_vmlinux1 .tmp_kallsyms1.o
 
 	# step 2
-	vmlinux_link .tmp_vmlinux2 .tmp_kallsyms1.o ${btf_vmlinux_bin_o}
+	vmlinux_link .tmp_kallsyms1.o .tmp_vmlinux2
 	kallsyms .tmp_vmlinux2 .tmp_kallsyms2.o
 
 	# step 3
@@ -425,7 +383,8 @@ if [ -n "${CONFIG_KALLSYMS}" ]; then
 		kallsymso=.tmp_kallsyms3.o
 		kallsyms_vmlinux=.tmp_vmlinux3
 
-		vmlinux_link .tmp_vmlinux3 .tmp_kallsyms2.o ${btf_vmlinux_bin_o}
+		vmlinux_link .tmp_kallsyms2.o .tmp_vmlinux3
+
 		kallsyms .tmp_vmlinux3 .tmp_kallsyms3.o
 	fi
 fi
@@ -447,7 +406,7 @@ if [ ! -z ${RTIC_MP_O} ]; then
 fi
 
 info LD vmlinux
-vmlinux_link vmlinux "${kallsymso}" "${btf_vmlinux_bin_o}"
+vmlinux_link "${kallsymso}" vmlinux
 
 if [ -n "${CONFIG_BUILDTIME_EXTABLE_SORT}" ]; then
 	info SORTEX vmlinux
