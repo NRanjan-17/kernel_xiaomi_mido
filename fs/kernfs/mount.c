2a2ede5794a88e41a77de412f8ba1052c8a31bfa
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-12 17:46:57 +0800

    fixup! "fix cgroup_do_mount() handling of failure exits"
    ref: https://github.com/torvalds/linux/commit/399504e21a10be16dd1408ba0147367d9d82a10c
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- fs/kernfs/mount.c
+++ fs/kernfs/mount.c
@@ -139,9 +139,8 @@ struct dentry *kernfs_node_dentry(struct kernfs_node *kn,
 			dput(dentry);
 			return ERR_PTR(-EINVAL);
 		}
-		mutex_lock(&d_inode(dentry)->i_mutex);
-		dtmp = lookup_one_len(kntmp->name, dentry, strlen(kntmp->name));
-		mutex_unlock(&d_inode(dentry)->i_mutex);
+		dtmp = lookup_one_len_unlocked(kntmp->name, dentry,
+					       strlen(kntmp->name));
 		dput(dentry);
 		if (IS_ERR(dtmp))
 			return dtmp;
